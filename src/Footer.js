2f78e39c8f796e3abfc0fe81e1474bb40a30e73a
Roshan Acharya <roshanacharya207@gmail.com>
2023-12-21 14:36:11 +0545

    MealAppAPI

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+function Footer() {
+  return (
+    <div style={{ backgroundColor: "grey" }}>
+      <h1>This is footer</h1>
+      <p style={{ color: "white" }}>Made by Roshan Acharya </p>
+    </div>
+  );
+}
+export default Footer;
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div style={{ backgroundColor: "Grey" }}>
+      {" "}
+      <h1>Navbar</h1>
+      <a style={{ margin: "10px", color: "white" }}>Home</a>
+      <a style={{ margin: "10px", color: "white" }}>Order</a>
+      <a style={{ margin: "10px", color: "white" }}>Contact Us</a>
+    </div>
+  );
+}
+export default Navbar;
